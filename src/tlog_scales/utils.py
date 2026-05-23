8ee1292ec27ecbeb60400a9dc60c09db56bdd870
Florian Larysch <fl@n621.de>
2026-05-23 17:52:15 +0200

    utils: revamp atomic_write
    Revamp atomic_write based on how the Tessera POSIX backend[1] does things. This is cargo cult
    to some extent but this is at least closer to something tested at scale
    on different file systems.
    
    [1] https://github.com/transparency-dev/tessera/blob/64486b7bdb392f5fc8617f19476b25354dee03e2/storage/posix/README.md

--- src/tlog_scales/utils.py
+++ src/tlog_scales/utils.py
@@ -44,18 +44,45 @@ def sync_write(dst: Path, data: bytes) -> None:
 def atomic_write(path: Path, data: bytes) -> None:
     path = path.resolve()
 
-    with tempfile.NamedTemporaryFile(mode="wb", dir=path.parent, delete=False) as f:
-        f.write(data)
-        f.flush()
-        os.fsync(f.fileno())
-        tmp = f.name
+    # Keep a reference to the parent dir for the duration of the operation. Tessera argues[1]:
+    #
+    #   This dance ensures that the inode of the specified directory cannot be
+    #   evicted from the kernel inode cache while the operation is underway,
+    #   and so any error which occurs while updating metadata about a file
+    #   operation which happens _within_ that directory is detected.
+    #
+    # [1] https://github.com/transparency-dev/tessera/blob/ab720fc8dc0e2ab7afcc41095c563d1e8f32384f/storage/posix/file_ops.go#L35-L39
+
+    parent = path.parent
+    dir_fd = os.open(parent, os.O_RDONLY | os.O_DIRECTORY)
 
     try:
-        os.replace(tmp, path)
-    except:
-        try:
-            os.remove(tmp)
-        except OSError:
-            pass
+        fd, tmp_path = tempfile.mkstemp(dir=parent, suffix=".tmp")
 
-        raise
+        try:
+            os.write(fd, data)
+            os.fsync(fd)
+        except:
+            os.close(fd)
+            try:
+                os.unlink(tmp_path)
+            except OSError:
+                pass
+
+            raise
+
+        os.close(fd)
+
+        try:
+            os.replace(tmp_path, path)
+        except:
+            try:
+                os.unlink(tmp_path)
+            except OSError:
+                pass
+
+            raise
+
+        os.fsync(dir_fd)
+    finally:
+        os.close(dir_fd)
